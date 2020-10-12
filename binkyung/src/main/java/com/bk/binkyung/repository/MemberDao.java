8b36dce5f1a1f1e191b4143417be577876a2cd21
UserK <UserK@Win10-01>
2020-10-12 19:48:43 +0900

    회원가입, 로그인, 로그아웃 복습완료

--- binkyung/src/main/java/com/bk/binkyung/repository/MemberDao.java
+++ binkyung/src/main/java/com/bk/binkyung/repository/MemberDao.java
@@ -5,8 +5,8 @@ import com.bk.binkyung.entity.MemberDto;
 public interface MemberDao {
 	
 	void member_join(MemberDto memberDto);
+
+	MemberDto find_id(String member_id);
 	
-	MemberDto member_select(String member_id);
-	
-	MemberDto member_login(MemberDto memberDto);
+	MemberDto member_find(MemberDto memberDto);
 }
